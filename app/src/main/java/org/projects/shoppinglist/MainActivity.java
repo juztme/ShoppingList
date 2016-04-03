2e9c4a560b6dc8ef0ec05874a6817ef64adda786
Marina <juztmejuztme@yahoo.com>
2016-04-03 13:21:04 +0200

    added icon for the clear all command in the activity bar

--- app/src/main/java/org/projects/shoppinglist/MainActivity.java
+++ app/src/main/java/org/projects/shoppinglist/MainActivity.java
@@ -168,7 +168,8 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+        //return true;
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
@@ -176,11 +177,34 @@ public class MainActivity extends AppCompatActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        /*int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
+        }*/
+
+        switch(item.getItemId()){
+            case R.id.clear_all:
+                //showing dialog before anything gets deleted
+                MyDialogFragment dialog = new MyDialogFragment(){
+                    @Override
+                    protected void positiveClick(){
+                        bag.clear();
+                        getMyAdapter().notifyDataSetChanged();
+                    }
+
+                    @Override
+                    protected void negativeClick(){
+                        Toast toast = Toast.makeText(getApplicationContext(), "List not cleared", Toast
+                                .LENGTH_LONG);
+                        toast.show();
+                    }
+                };
+                dialog.show(getFragmentManager(), "MyFragment");
+                return true;
+            case R.id.action_settings:
+                return true;
         }
 
         return super.onOptionsItemSelected(item);
